d56a4fd965d793318443a9bee9640118102bca13
Mizumoto <mizumotokunn@gmail.com>
2022-07-23 00:37:41 +0900

    updated more functinoal way

--- JavaTut/Lambda/Factorial.java
+++ JavaTut/Lambda/Factorial.java
@@ -5,6 +5,17 @@ public class Factorial {
         return (n == 1) ? 1 : n * factorial(n - 1);
     }
 
+    private interface IAcc{
+        int acc(int n, int acc);
+    }
+
+    private static int factorial_2(int n) {
+        IAcc acc = (int n, int acc) -> {
+            return (n == 1) ? acc : factorial_2(n - 1);
+        };
+        return acc.acc(n, 1);
+    }
+
     public static void main(String[] args) {
         System.out.println(factorial(10));
     }
